b10eb8fee5c39a125401299c2b32b0992095059c
Chahin Taher <151779945+Chahin18@users.noreply.github.com>
2025-01-20 12:29:00 +0100

    Update request.js

--- frontend/src/utils/request.js
+++ frontend/src/utils/request.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const request = axios.create({
-    baseURL: "http://localhost:8000"
+    baseURL: "https://project-blog-mern-ev4e.vercel.app"
 });
 
 export default request;
